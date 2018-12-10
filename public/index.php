a3e61f337bfe682d9159b5f0f909b573fcc14a47
Gregg Christofferson <gechristofferson@gmail.com>
2018-12-10 23:28:18 +0000

    First commit

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php require_once '../vendor/autoload.php'; echo "Hello World";
+
+$database = new \Medoo\Medoo([ 'database_type' => 'sqlite', 'database_file' => '../storage/database.db' ]);
+dump($database);
